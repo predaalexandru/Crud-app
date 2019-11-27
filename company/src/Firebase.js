a2d9e975fd20cda6c71e5c9afc938b0be3b0da91
predaalexandru <alexpreda95@yahoo.com>
2019-11-27 20:57:21 +0200

    Add the structure project, bootstrap and connection to firebase

---
+++ company/src/Firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+import firestore from 'firebase/firestore'
+
+const settings = {timestampsInSnapshots: true};
+
+const config = {
+  apiKey: "AIzaSyDxSTrEd6SU2vH1LB9UWIdZm-FdYNAQk9c",
+  authDomain: "company-86613.firebaseapp.com",
+  databaseURL: "https://company-86613.firebaseio.com",
+  projectId: "company-86613",
+  storageBucket: "company-86613.appspot.com",
+  messagingSenderId: "645825261448"
+};
+firebase.initializeApp(config);
+
+firebase.firestore().settings(settings);
+
+export default firebase;
